f6aebe6c077a1ce48bd64a3f2397e10cd47859d4
teaglebuilt <dillan.teagle.va@gmail.com>
2018-04-17 13:59:26 -0500

    changes tupac

--- database.js
+++ database.js
@@ -7,13 +7,17 @@ rainyMolesDatabase.financialInterests = []
 
 rainyMolesDatabase.summary.push(
 {
-    status: "Very Dangerous",
+    Status: "Very Dangerous",
     Name: "Edgar Valdez Villarreal",
     Image: "https://media.npr.org/assets/img/2010/09/09/barbie-f412f0a2b3f4b2e80abc94eafd36c843c3102048-s300-c85.jpg",
     CountryofOrigin: "United States",
     Location: "Mexico",
-    KnownAlias: "La Barbie"
+    KnownAlias: "La Barbie",
+    Description: "Edgar Valdez Villarreal also known as La Barbie, is a Mexican American suspected drug lord and leader of the Beltrán Leyva Cartel, a criminal group based in Sinaloa. Edgar is currently on the run after breaking out of United States Federal Extradition holding facility in Mexico City."
+
 },
+    
+     
 
 {
     Associate1: {
@@ -160,5 +164,8 @@ rainyMolesDatabase.newsFeed.push(
     Witnesses: '0',
     Law_Enforcement: 'Mexican Police',
 })
+
+
+
 
 localStorage.setItem("rainyMolesDatabase", JSON.stringify(rainyMolesDatabase))
--- summary.js
+++ summary.js
@@ -1,5 +1,48 @@
-                   
-                     
-                     
-                 
+const test = document.querySelector("#test")
 
+const buildSummary = () => {
+
+    const container =  document.createElement('section')
+                   container.classList.add("suspect-home")
+                   test.appendChild(container)
+
+    const dangerous = document.createElement("h1")
+                   dangerous.classList.add("Danger")
+                   dangerous.textContent = rainyMolesDatabase.summary[0].Status
+                   container.appendChild(dangerous)
+
+     const name = document.createElement("h2")
+                  name.classList.add("suspect-name")  
+                  name.textContent = rainyMolesDatabase.summary[0].Name
+                  container.appendChild(name)      
+                  
+     const image = document.createElement("img")
+                  image.classList.add("suspect-img")
+                  image.src = rainyMolesDatabase.summary[0].Image
+                  container.appendChild(image)  
+
+     const origin = document.createElement("h3")
+                    origin.classList.add("country")
+                    origin.innerHTML += "Country of Origin: <span class= 'Geo Danger'>" + rainyMolesDatabase.summary[0].CountryofOrigin + '</span>'
+                    container.appendChild(origin)
+
+    const location = document.createElement("h3")
+                     location.classList.add("country")
+                     location.innerHTML += "Country Location: <span class= 'Geo Green'>" + rainyMolesDatabase.summary[0].Location + '</span>'
+                     container.appendChild(location)
+                 
+      const alias = document.createElement("h3")
+                    alias.classList.add("country")
+                    alias.textContent = "Know Alias: " + rainyMolesDatabase.summary[0].KnownAlias
+                    container.appendChild(alias)
+
+      const description = document.createElement('p')
+                          description.classList.add("descr Aldrich")
+                          description.textContent = rainyMolesDatabase.summary[0].Description
+                          container.appendChild(description)
+}
+
+
+buildSummary()    
+     
+     
