f73197691127cfa6a78d757ab7fdff479807f0f6
Jessica Swift <jessica.nicole.swift@gmail.com>
2018-04-17 15:29:19 -0500

    still not working but getting there.

--- newsfeed.js
+++ newsfeed.js
@@ -1,11 +1,14 @@
 const newsFeedRef = document.querySelector("newsPage")
 
 
-const newsDomBuilder = () => {
+const newsDomBuilder = (function(newsObj) {
 
     // Creating block element
     let row = document.createElement('div');
         row.classList.add = 'newsRow';
+
+        newsFeedRef.appendChild(row)
+        row = document.createElement('article')
 
         rainyMolesDatabase.newsFeed.forEach();
 
@@ -22,11 +25,23 @@ const newsDomBuilder = () => {
     let jsnewsArticle = document.createElement('a');
     jsnewsArticle.setAttribute('newsarticle');
     jsnewsArticle.setAttribute('href');
-    newsDiv.appendChild(jsnewsArticle);
+    newsDiv.appendChild(newsObj.article);
 
     // description
-    let jsArticleDescription = document.createElement('div');
+    let jsArticleDescription = document.createElement('h4');
     jsArticleDescription.classList.add('align');
+    jsArticleDescription.textContent = articlediv.description;
+    newsDiv.appendChild(jsArticleDescription);
+
+    // news article website
+    let ulDiv = document.createElement('div');
+    ulDiv.classList.add('article-container');
+    newsDiv.appendChild(ulDiv);
+
+    let ulNews = document.createElement('ul');
+
+        let liRate = document.createElement('li');
     
 }
 
+console.log('hello world')
