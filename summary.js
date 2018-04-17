12d20ef475785d5f662f408bc79adfaac91c3e81
teaglebuilt <dillan.teagle.va@gmail.com>
2018-04-17 08:12:42 -0500

    summary data

---
+++ summary.js
@@ -0,0 +1,39 @@
+const rainyMolesDatabase = {}
+
+rainyMolesDatabase.summary = []
+rainyMolesDatabase.newsFeed = []
+rainyMolesDatabase.rapSheet = []
+rainyMolesDatabase.financialInterests = []
+
+rainyMolesDatabase.summary.push(
+{
+    status: "Very Dangerous",
+    Name: "Edgar Valdez Villarreal",
+    Image: "https://media.npr.org/assets/img/2010/09/09/barbie-f412f0a2b3f4b2e80abc94eafd36c843c3102048-s300-c85.jpg",
+    CountryofOrigin: "United States",
+    Location: "Mexico",
+    KnownAlias: "La Barbie"
+},
+
+{
+    Associates: [{
+                Image: "https://s.hdnux.com/photos/04/52/43/1222889/3/920x920.jpg",
+                Name: "Gerardo Alvarez-Vazquez",
+                Description: "A Mexican drug trafficker who worked for the Beltrán-Leyva Cartel and then for Édgar Valdez Villarreal's criminal organization known as Los Negros. In 2008, the United States offered a 2 million dollar reward for his capture."
+                 },
+                 {
+                 Image: "http://thehill.com/sites/default/files/styles/thumb_small_article/public/blogs/bushgeorgew_120714getty.jpg?itok=6Xq7fdkZ",
+                 Name: "George W. Bush",
+                 Description: "Former Republican President, his ties with Edgar stem from his private passion for good old fashioned grade A chronic, the way it was back when. As Edgar headed into harder drugs with higher profit margins, his political connections started to vanish."
+                 },
+                 {
+                 Image: "https://i1.wp.com/www.pbs.org/wgbh/frontline/wp-content/uploads/2015/11/1441750516_AP_090312065379-1024x801.jpg",
+                 Name: "Joaquín Guzmán",
+                 Description: "In the past few years, Guzman has risen to become one of the most notorious drug lords in Mexico. Head of the Sinoloa Cartel, named off of the Mexican Pacific coast the State of Sinoloa where the Cartel was originally formed."
+                 }]              
+}
+)                     
+                     
+                     
+                 
+
